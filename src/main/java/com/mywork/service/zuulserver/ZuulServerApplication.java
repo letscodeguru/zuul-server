3832d97c5d9ded6bd42c56c568699e115bb99eab
kunal Chaudhari <kunalpict@gmail.com>
2018-04-15 19:23:51 -0400

    updated to run in tomcat

--- src/main/java/com/mywork/service/zuulserver/ZuulServerApplication.java
+++ src/main/java/com/mywork/service/zuulserver/ZuulServerApplication.java
@@ -2,11 +2,20 @@ package com.mywork.service.zuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.web.ZuulController;
 
 @EnableZuulProxy
 @SpringBootApplication
-public class ZuulServerApplication {
+public class ZuulServerApplication   extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ZuulServerApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulServerApplication.class, args);
